167275dc19f12f935ee0222a2d171686c9368319
Will Soto <willsoto@users.noreply.github.com>
2021-12-05 20:20:20 -0500

    feat(day_1): implement part 2
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- 2021/src/day_1/mod.rs
+++ 2021/src/day_1/mod.rs
@@ -1,31 +1,65 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[allow(dead_code)]
-pub fn run() {
-  let reader = BufReader::new(
+fn read_input_file() -> BufReader<File> {
+  return BufReader::new(
     File::open("./src/day_1/input.txt").expect("Something went wrong reading the file"),
   );
+}
+
+#[allow(dead_code)]
+pub fn run_part_1() {
+  let reader = read_input_file();
 
   let mut previous: i32 = 0;
   let mut total_larger: i32 = 0;
 
   for line in reader.lines() {
-    for value in line.unwrap().split_whitespace() {
-      let current: i32 = value.parse().unwrap();
+    let current: i32 = line.unwrap().parse().unwrap();
 
-      if previous == 0 {
-        previous = current;
-        continue;
-      }
-
-      if current > previous {
-        total_larger = total_larger + 1;
-      }
-
+    if previous == 0 {
       previous = current;
+      continue;
     }
+
+    if current > previous {
+      total_larger = total_larger + 1;
+    }
+
+    previous = current;
   }
 
   assert_eq!(total_larger, 1228);
+}
+
+#[allow(dead_code)]
+pub fn run_part_2() {
+  let reader = read_input_file();
+  let window_size = 3;
+
+  let mut previous: i32 = 0;
+  let mut total_larger: i32 = 0;
+  let mut processed_lines: Vec<i32> = vec![];
+
+  for line in reader.lines() {
+    let value: i32 = line.unwrap().parse().unwrap();
+    processed_lines.push(value);
+  }
+
+  for window in processed_lines.windows(window_size) {
+    let current: i32 = window.iter().sum();
+
+    if previous == 0 {
+      previous = current;
+      continue;
+    }
+
+    if current > previous {
+      total_larger = total_larger + 1;
+    }
+
+    previous = current;
+  }
+
+  assert_eq!(total_larger, 1257);
 }
--- 2021/src/lib.rs
+++ 2021/src/lib.rs
@@ -6,6 +6,7 @@ mod tests {
 
     #[test]
     fn day_1() {
-        day_1::run();
+        day_1::run_part_1();
+        day_1::run_part_2();
     }
 }
