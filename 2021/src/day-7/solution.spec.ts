4876f148cd41d7d182c4016e9a15db20927fae32
Will Soto <willsoto@users.noreply.github.com>
2021-12-19 20:57:22 -0500

    feat(day-7): implement day 7
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

---
+++ 2021/src/day-7/solution.spec.ts
@@ -0,0 +1,14 @@
+import test from "ava";
+import { part1, part2 } from "./solution.js";
+
+test("part 1", (t) => {
+  const result = part1();
+
+  t.is(result, 355521);
+});
+
+test("part 2", (t) => {
+  const result = part2();
+
+  t.is(result, 100148777);
+});
---
+++ 2021/src/day-7/solution.ts
@@ -0,0 +1,52 @@
+import { openInput } from "../utils.js";
+
+export function part1(): number {
+  const positions = getHorizontalPositions();
+
+  let midpoint = positions[Math.floor(positions.length / 2)];
+  let cost = 0;
+
+  positions.forEach((position) => {
+    cost += Math.abs(position - midpoint);
+  });
+
+  if (positions.length % 2 === 0) {
+    let otherCost = 0;
+    let midpoint = positions[Math.floor((positions.length - 1) / 2)];
+
+    positions.forEach((position) => {
+      otherCost += Math.abs(position - midpoint);
+    });
+
+    cost = Math.min(cost, otherCost);
+  }
+
+  return cost;
+}
+
+export function part2(): number {
+  const positions = getHorizontalPositions();
+
+  let minFuel = Infinity;
+  for (let i = 0; i < positions.length; i++) {
+    let fuel = positions.reduce((accum, position) => {
+      const difference = Math.abs(position - i);
+      const distance = (difference * (difference + 1)) / 2;
+
+      return accum + distance;
+    }, 0);
+
+    if (fuel < minFuel) {
+      minFuel = fuel;
+    }
+  }
+
+  return minFuel;
+}
+
+function getHorizontalPositions() {
+  return openInput(import.meta.url)
+    .split(",")
+    .map((line) => parseInt(line))
+    .sort((a, b) => a - b);
+}
